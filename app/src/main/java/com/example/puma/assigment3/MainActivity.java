604992a50dc20475a527e156b1678f18407f2727
ingamontclair <gaidukovai1@montclair.edu>
2017-03-18 15:16:35 -0400

    Initial commit

---
+++ app/src/main/java/com/example/puma/assigment3/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.puma.assigment3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button buttonStart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //defining button for starting a game
+        buttonStart = (Button)findViewById(R.id.btn_start);
+        buttonStart.setOnClickListener(new StartLstr());
+
+    }
+    class StartLstr implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.btn_start) {
+                Intent intent = new Intent(MainActivity.this, WorkingArea.class);
+                startActivity(intent);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/puma/assigment3/WorkingArea.java
@@ -0,0 +1,13 @@
+package com.example.puma.assigment3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkingArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_working_area);
+    }
+}
