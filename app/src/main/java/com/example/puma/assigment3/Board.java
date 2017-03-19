4a8503b503ef4f5742cb8faf89942b2aef318ec9
ingamontclair <inga_bv@mail.ru>
2017-03-19 12:13:32 -0400

    game board interface added

--- app/src/main/java/com/example/puma/assigment3/Board.java
+++ app/src/main/java/com/example/puma/assigment3/Board.java
@@ -1,8 +1,32 @@
 package com.example.puma.assigment3;
 
-/**
- * Created by Alex on 3/19/2017.
- */
+import java.util.List;
 
 public class Board {
+
+    public Board(int boardResourceId, int[] startingPosition, int[] endPosition, int[][] map) {
+
+
+    }
+
+    public static Board MAP_1 = new Board(
+            R.drawable.s101,
+            new int[]{0, 1},
+            new int[]{7, 5},
+            new int[][]{
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 1, 1, 0, 0, 0, 0},
+                    {0, 0, 0, 1, 0, 0, 0, 0},
+                    {0, 0, 0, 1, 0, 0, 0, 0},
+                    {0, 0, 0, 1, 1, 1, 1, 1},
+                    {0, 0, 0, 0, 0, 0, 0, 0}
+            }
+
+    );
+
+
+    public boolean run (List<Direction> instructions) {
+        return true;
+    }
+
 }
---
+++ app/src/main/java/com/example/puma/assigment3/Direction.java
@@ -0,0 +1,9 @@
+package com.example.puma.assigment3;
+
+/**
+ * Created by Inga on 3/19/2017.
+ */
+
+public enum Direction {
+    LEFT, RIGHT, UP, DOWN;
+}
