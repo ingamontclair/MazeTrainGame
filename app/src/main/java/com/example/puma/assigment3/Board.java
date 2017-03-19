350481b0295b1317bd7bfc1864195102947108e7
ingamontclair <inga_bv@mail.ru>
2017-03-19 12:37:56 -0400

    game board interface added

--- app/src/main/java/com/example/puma/assigment3/Board.java
+++ app/src/main/java/com/example/puma/assigment3/Board.java
@@ -1,8 +1,15 @@
 package com.example.puma.assigment3;
 
+import android.widget.ImageView;
+
 import java.util.List;
 
 public class Board {
+
+    private int top;
+    private int left;
+    private int width;
+    private int length;
 
     public Board(int boardResourceId, int[] startingPosition, int[] endPosition, int[][] map) {
 
@@ -21,11 +28,19 @@ public class Board {
                     {0, 0, 0, 1, 1, 1, 1, 1},
                     {0, 0, 0, 0, 0, 0, 0, 0}
             }
-
     );
 
+    public void setScreenDimensions(int top, int left, int width, int length) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.length = length;
+    }
 
-    public boolean run (List<Direction> instructions) {
+    public boolean run(ImageView train, List<Direction> instructions) {
+        if (width == 0 || left == 0)
+            throw new RuntimeException("Screen dimensions are not set");
+
         return true;
     }
 
--- app/src/main/java/com/example/puma/assigment3/WorkingArea.java
+++ app/src/main/java/com/example/puma/assigment3/WorkingArea.java
@@ -3,9 +3,12 @@ package com.example.puma.assigment3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.ViewTreeObserver;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import java.util.Arrays;
 
 public class WorkingArea extends AppCompatActivity {
 
@@ -15,20 +18,24 @@ public class WorkingArea extends AppCompatActivity {
         setContentView(R.layout.activity_working_area);
 
         ImageView train = (ImageView) findViewById( R.id.train );
-        LinearLayout board = (LinearLayout) findViewById( R.id.activity_working_area );
-        DisplayMetrics dm = new DisplayMetrics();
-        this.getWindowManager().getDefaultDisplay().getMetrics( dm );
-        int statusBarOffset = dm.heightPixels - board.getMeasuredHeight();
+        final LinearLayout workingAreaView = (LinearLayout) findViewById( R.id.activity_working_area );
+        final int[] size = new int[2];
 
-        int originalPos[] = new int[2];
-        train.getLocationOnScreen( originalPos );
+        ViewTreeObserver vto = workingAreaView.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                workingAreaView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                size[0]  = workingAreaView.getMeasuredWidth();
+                size[1] = workingAreaView.getMeasuredHeight();
+            }
+        });
 
-        int xDest = dm.widthPixels/4;
-        xDest -= (train.getMeasuredWidth()/2);
+        Board b = Board.MAP_1;
+        b.setScreenDimensions(workingAreaView.getTop(), workingAreaView.getLeft(), size[0], size[1]);
+        boolean result = b.run(train, Arrays.asList(Direction.RIGHT, Direction.DOWN, Direction.RIGHT));
 
-        TranslateAnimation anim = new TranslateAnimation( 0, xDest - originalPos[0] , 0, originalPos[1] );
-        anim.setDuration(5000);
-        anim.setFillAfter( true );
-        train.startAnimation(anim);
+        System.out.println("Finished: " + result);
+
     }
 }
